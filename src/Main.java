28cb40822cc4663367492e1acfaa7084cc179cfd
Igor <igormedeiros407@gmail.com>
2024-07-14 18:01:06 -0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int n1, n2;
+        n1 = 4;
+        n2= 4;
+        if (n1 == n2){
+            System.out.println("igual pois n1 é igual n2, n1= " + n1 + " e n2 = " +n2);
+        }else if (n1 != n2){
+            System.out.println("n1 é diferente de n2 porque a sobra foi de " + (n2 - n1 ));
         }
+
     }
 }
